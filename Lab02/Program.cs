6685842339bba74570f1954c607b6fa92c176ccc
weerapat <joxxt007@gmail.com>
2026-08-24 11:00:05 +0700

    Update Program.cs

--- Lab02/Program.cs
+++ Lab02/Program.cs
@@ -4,7 +4,38 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string bossName = "Kirin";
+            char rank = 'S';
+            int level = 7;
+            int maxHp = 240;
+            int currentHp = 175;
+            float attackPower = 42.5f;
+            double critMultiplier = 1.75;
+            bool isBoss = true;
+
+            Console.WriteLine("===== BOSS STATUS: INITIAL =====");
+            Console.WriteLine($"Name: {bossName}");
+            Console.WriteLine($"Rank: {rank}");
+            Console.WriteLine($"Level: {level}");
+            Console.WriteLine($"Max HP: {maxHp}");
+            Console.WriteLine($"Current HP: {currentHp}");
+            Console.WriteLine($"Attack Power: {attackPower}");
+            Console.WriteLine($"Critical Multiplier: {critMultiplier}");
+            Console.WriteLine($"Is Boss: {isBoss}");
+            int hpPercent = currentHp * 100 / maxHp;
+
+            Console.WriteLine();
+            Console.WriteLine($"HP Percent: {hpPercent}%");
+
+            Console.WriteLine();
+            Console.WriteLine($"{bossName} takes 60 damage!");
+            currentHp = currentHp - 60;
+
+            Console.WriteLine();
+            Console.WriteLine("===== BOSS STATUS: AFTER DAMAGE =====");
+            Console.WriteLine($"HP: {currentHp} / {maxHp}");
+            hpPercent = currentHp * 100 / maxHp;
+            Console.WriteLine($"HP Percent: {hpPercent}%");
         }
     }
 }
